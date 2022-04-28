39bfe32c5a2d7c940a836433f6427881086d6a0c
UnusualMessage <nikita_kot2@mail.ru>
2022-04-28 19:20:04 +0300

    Объединил все компоненты

--- sushi-front/src/App.js
+++ sushi-front/src/App.js
@@ -1,11 +1,13 @@
-import { Routes } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+
+import Main from "./Main/Main";
 
 import './App.scss';
 
 const App = () => {
     return (
         <Routes>
-
+            <Route path="/" element={<Main/>}/>
         </Routes>
     )
 }
---
+++ sushi-front/src/Main/Main.js
@@ -0,0 +1,17 @@
+import Header from "./Components/Base/Header/Header";
+import Nav from "./Components/Base/Nav/Nav";
+import Footer from "./Components/Base/Footer/Footer";
+import Shopping from "./Components/Pages/Shopping/Shopping";
+
+const Main = () => {
+    return(
+        <>
+            <Header/>
+            <Nav/>
+            <Shopping/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Main;
