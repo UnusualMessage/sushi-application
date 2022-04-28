60022a365421c24dcb5912530d0d7280b0504af2
UnusualMessage <nikita_kot2@mail.ru>
2022-04-28 19:19:41 +0300

    Добавил страницу с товарами

---
+++ sushi-front/src/Main/Components/Pages/Shopping/Shopping.js
@@ -0,0 +1,207 @@
+import './Shopping.scss';
+
+const Shopping = (props) => {
+    return(
+        <>
+            <main className='main'>
+                <div className='main-wrapper'>
+                    <div className='main-content'>
+                        <div className='shopping-card'>
+                            <div className='card-image'>
+                                <img src="/1.jfif" alt=""/>
+                            </div>
+
+                            <div className='card-description'>
+                                <span className='card-description-title'>
+                                    Пирожное что пву
+                                </span>
+
+                                <div className='card-description-order'>
+                                    <span className='order-price'>
+                                        300
+                                    </span>
+
+                                    <div className='order-button'>
+                                        <span className='order-button-text'>
+                                            Добавить
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='shopping-card'>
+                            <div className='card-image'>
+                                <img src="/2.jfif" alt=""/>
+                            </div>
+
+                            <div className='card-description'>
+                                <span className='card-description-title'>
+                                    Пирожное что пву
+                                </span>
+
+                                <div className='card-description-order'>
+                                    <span className='order-price'>
+                                        300
+                                    </span>
+
+                                    <div className='order-button'>
+                                        <span className='order-button-text'>
+                                            Добавить
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='shopping-card'>
+                            <div className='card-image'>
+                                <img src="/3.jfif" alt=""/>
+                            </div>
+
+                            <div className='card-description'>
+                                <span className='card-description-title'>
+                                    Пирожное что пву
+                                </span>
+
+                                <div className='card-description-order'>
+                                    <span className='order-price'>
+                                        300
+                                    </span>
+
+                                    <div className='order-button'>
+                                        <span className='order-button-text'>
+                                            Добавить
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='shopping-card'>
+                            <div className='card-image'>
+                                <img src="/3.jfif" alt=""/>
+                            </div>
+
+                            <div className='card-description'>
+                                <span className='card-description-title'>
+                                    Пирожное что пву
+                                </span>
+
+                                <div className='card-description-order'>
+                                    <span className='order-price'>
+                                        300
+                                    </span>
+
+                                    <div className='order-button'>
+                                        <span className='order-button-text'>
+                                            Добавить
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='shopping-card'>
+                            <div className='card-image'>
+                                <img src="/3.jfif" alt=""/>
+                            </div>
+
+                            <div className='card-description'>
+                                <span className='card-description-title'>
+                                    Пирожное что пву
+                                </span>
+
+                                <div className='card-description-order'>
+                                    <span className='order-price'>
+                                        300
+                                    </span>
+
+                                    <div className='order-button'>
+                                        <span className='order-button-text'>
+                                            Добавить
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='shopping-card'>
+                            <div className='card-image'>
+                                <img src="/3.jfif" alt=""/>
+                            </div>
+
+                            <div className='card-description'>
+                                <span className='card-description-title'>
+                                    Пирожное что пву
+                                </span>
+
+                                <div className='card-description-order'>
+                                    <span className='order-price'>
+                                        300
+                                    </span>
+
+                                    <div className='order-button'>
+                                        <span className='order-button-text'>
+                                            Добавить
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='shopping-card'>
+                            <div className='card-image'>
+                                <img src="/3.jfif" alt=""/>
+                            </div>
+
+                            <div className='card-description'>
+                                <span className='card-description-title'>
+                                    Пирожное что пву
+                                </span>
+
+                                <div className='card-description-order'>
+                                    <span className='order-price'>
+                                        300
+                                    </span>
+
+                                    <div className='order-button'>
+                                        <span className='order-button-text'>
+                                            Добавить
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='shopping-card'>
+                            <div className='card-image'>
+                                <img src="/3.jfif" alt=""/>
+                            </div>
+
+                            <div className='card-description'>
+                                <span className='card-description-title'>
+                                    Пирожное что пву
+                                </span>
+
+                                <div className='card-description-order'>
+                                    <span className='order-price'>
+                                        300
+                                    </span>
+
+                                    <div className='order-button'>
+                                        <span className='order-button-text'>
+                                            Добавить
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Shopping;
