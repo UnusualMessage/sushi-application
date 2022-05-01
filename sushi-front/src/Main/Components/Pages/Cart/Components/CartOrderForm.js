5b013baf3ca7e3a4aa3c88f3873d41c3f9c4f3dc
UnusualMessage <nikita_kot2@mail.ru>
2022-05-01 19:57:49 +0300

    Базовая реализация формы для корзины

--- sushi-front/src/Main/Components/Pages/Cart/Components/CartOrderForm.js
+++ sushi-front/src/Main/Components/Pages/Cart/Components/CartOrderForm.js
@@ -1,8 +1,55 @@
+import "../Styles/CartOrderForm.scss";
+
 const CartOrderForm = () => {
-    return(
+    return (
         <>
             <section className="cart-order-form">
+                <div className="cart-order-form-result">
+                    <span className="cart-order-form-result-text">Итого:</span>
+                    <span className="cart-order-form-result-price">512</span>
+                </div>
 
+                <div className="cart-order-form-who">
+                    <span className="cart-order-form-who-title">Кому доставить</span>
+                    <div className="cart-order-form-who-phone">
+                        <span>Телефон:</span>
+                        <input></input>
+                    </div>
+
+                    <div className="cart-order-form-who-name">
+                        <span>Как вас назвать:</span>
+                        <input></input>
+                    </div>
+                </div>
+
+                <div className="cart-order-form-address">
+                    <span className="cart-order-form-address-title">Адрес доставки</span>
+
+                    <div className="cart-order-form-address-street">
+                        <span>Улица:</span>
+                        <input />
+                    </div>
+
+                    <div className="cart-order-form-address-home">
+                        <span>Номер дома:</span>
+                        <input />
+                    </div>
+
+                    <div className="cart-order-form-address-part">
+                        <span>Подъезд:</span>
+                        <input />
+                    </div>
+
+                    <div className="cart-order-form-address-floor">
+                        <span>Этаж:</span>
+                        <input />
+                    </div>
+
+                    <div className="cart-order-form-address-flat">
+                        <span>Квартира:</span>
+                        <input />
+                    </div>
+                </div>
             </section>
         </>
     );
