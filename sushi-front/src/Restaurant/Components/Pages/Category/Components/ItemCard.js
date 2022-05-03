668b1ff45a2f67e4536e77db208df1f97543f180
UnusualMessage <nikita_kot2@mail.ru>
2022-05-03 11:47:34 +0300

    Исправил баги с футером и сверстал страницу товара

--- sushi-front/src/Restaurant/Components/Pages/Category/Components/ItemCard.js
+++ sushi-front/src/Restaurant/Components/Pages/Category/Components/ItemCard.js
@@ -1,11 +1,9 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Cart from "../../../Store/Cart";
 
 import "../Styles/ItemCard.scss";
 
 const ItemCard = (props) => {
-    const navigate = useNavigate();
-
     const onClick = (e) => {
         e.preventDefault();
         Cart.add(props);
--- sushi-front/src/Restaurant/Components/Pages/Category/Components/ItemPage.js
+++ sushi-front/src/Restaurant/Components/Pages/Category/Components/ItemPage.js
@@ -1,26 +1,36 @@
 import { useParams } from "react-router-dom";
 import ShoppingData from "../../../Data/ShoppingData";
 
+import "../Styles/ItemPage.scss";
+
 const ItemPage = () => {
     const { category, id } = useParams();
     const item = ShoppingData.find((i) => (i.id.toString() === id) && (i.category === category));
-    console.log(category);
+
     return (
         <section className="item-page">
-            <div>
+            <div className="item-page-image">
                 <img src={item?.path} alt="" />
             </div>
 
-            <div>
-                {item?.title}
-            </div>
+            <div className="item-page-description">
+                <h1 className="item-page-description-title">
+                    {item?.title}
+                </h1>
 
-            <div>
-                {item?.text}
-            </div>
+                <div className="item-page-description-order">
+                    <span className="order-price">
+                        {item?.price}
+                    </span>
 
-            <div>
-                {item?.price}
+                    <span className="order-button">
+                        ДОБАВИТЬ
+                    </span>
+                </div>
+
+                <div className="item-page-description-text">
+                    {item?.text}
+                </div>
             </div>
         </section>
     );
